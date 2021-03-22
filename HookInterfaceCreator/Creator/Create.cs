ea0bd4aa0cf68eb6a4836b1208ae75b23030ccc2
LostVirt <rsvirtest@hotmail.com>
2021-03-22 19:12:46 +0100

    Added Node Deque Field

--- HookInterfaceCreator/Creator/Create.cs
+++ HookInterfaceCreator/Creator/Create.cs
@@ -64,6 +64,11 @@ namespace HookInterfaceCreator.Creator
                     AddExtraSceneFields(file);
                     file.WriteLine("");
                 }
+                else if (parent.Clazz.Equals("NodeDeque"))
+                {
+                    AddExtraNodeDequeFields(file);
+                    file.WriteLine("");
+                }
                      
                 if (parent.Fields != null)
                 {
@@ -80,11 +85,11 @@ namespace HookInterfaceCreator.Creator
 
                         if (!Utils.IsBaseType(descriptor))
                         {
-                            file.WriteLine($"    {Utils.GetJavaArrayString(arrayCount, FindInterface(returnType))} get{field.Field.FirstCharToUpper()}();");
+                            file.WriteLine(InlineWhiteSpace(1) + $"{Utils.GetJavaArrayString(arrayCount, FindInterface(returnType))} get{field.Field.FirstCharToUpper()}();");
                         }
                         else
                         {
-                            file.WriteLine($"    {Utils.GetJavaArrayString(arrayCount, returnType)} get{field.Field.FirstCharToUpper()}();");
+                            file.WriteLine(InlineWhiteSpace(1) + $"{Utils.GetJavaArrayString(arrayCount, returnType)} get{field.Field.FirstCharToUpper()}();");
                         }
                     }
                 }
@@ -97,40 +102,59 @@ namespace HookInterfaceCreator.Creator
 
         private void AddExtraClientFields(StreamWriter file)
         {
-            string whiteSpace = "    ";
-            file.WriteLine(whiteSpace + "Thread getGameThread();");
-            file.WriteLine(whiteSpace + "int getMouseX();");
-            file.WriteLine(whiteSpace + "int getMouseY();");
-            file.WriteLine(whiteSpace + "void sendKeyEventPressed(KeyEvent keyEvent);");
-            file.WriteLine(whiteSpace + "void sendKeyEventTyped(KeyEvent keyEvent);");
-            file.WriteLine(whiteSpace + "void sendKeyEventReleased(KeyEvent keyEvent);");
-            file.WriteLine(whiteSpace + "void sendMouseEventPressed(MouseEvent mouseEvent);");
-            file.WriteLine(whiteSpace + "void sendMouseEventReleased(MouseEvent mouseEvent);");
-            file.WriteLine(whiteSpace + "void sendMouseEventMoved(MouseEvent mouseEvent);");
-            file.WriteLine(whiteSpace + "void setAction(RSMenuAction menuAction);");
-            file.WriteLine(whiteSpace + "void setDisableAll(boolean bool);");
-            file.WriteLine(whiteSpace + "RSInventory getItemContainer(long tableID);");
-            file.WriteLine(whiteSpace + "int getVarbit(int index);");
-            file.WriteLine(whiteSpace + "void login(String username, String password);");
-            file.WriteLine(whiteSpace + "void setWorld(int id);");
-            file.WriteLine(whiteSpace + "boolean loadWorlds();");
-            file.WriteLine(whiteSpace + "void setWorldSelect(boolean bool);");
-            file.WriteLine(whiteSpace + "boolean getWorldSelect();");
-            file.WriteLine(whiteSpace + "void addMessage(int type, String sender, String text, String prefix);");
-            file.WriteLine(whiteSpace + "void doAction(int secondary, int tertiary, int opcode, int primary, String menuOption, String menuTarget, int mouseX, int mouseY, int trash);");
-            file.WriteLine(whiteSpace + "RSObjType getItemDefinition(int itemID);");
-            file.WriteLine(whiteSpace + "RSComponent getMeSlayerContinueWidget();");
-            file.WriteLine(whiteSpace + "void setScene_selectedX(int x);");
-            file.WriteLine(whiteSpace + "void setScene_selectedY(int y);");
-            file.WriteLine(whiteSpace + "void setViewportWalking();");
+            file.WriteLine(InlineWhiteSpace(1) + "Thread getGameThread();");
+            file.WriteLine(InlineWhiteSpace(1) + "int getMouseX();");
+            file.WriteLine(InlineWhiteSpace(1) + "int getMouseY();");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendKeyEventPressed(KeyEvent keyEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendKeyEventTyped(KeyEvent keyEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendKeyEventReleased(KeyEvent keyEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendMouseEventPressed(MouseEvent mouseEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendMouseEventReleased(MouseEvent mouseEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void sendMouseEventMoved(MouseEvent mouseEvent);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setAction(RSMenuAction menuAction);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setDisableAll(boolean bool);");
+            file.WriteLine(InlineWhiteSpace(1) + "RSInventory getItemContainer(long tableID);");
+            file.WriteLine(InlineWhiteSpace(1) + "int getVarbit(int index);");
+            file.WriteLine(InlineWhiteSpace(1) + "void login(String username, String password);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setWorld(int id);");
+            file.WriteLine(InlineWhiteSpace(1) + "boolean loadWorlds();");
+            file.WriteLine(InlineWhiteSpace(1) + "void setWorldSelect(boolean bool);");
+            file.WriteLine(InlineWhiteSpace(1) + "boolean getWorldSelect();");
+            file.WriteLine(InlineWhiteSpace(1) + "void addMessage(int type, String sender, String text, String prefix);");
+            file.WriteLine(InlineWhiteSpace(1) + "void doAction(int secondary, int tertiary, int opcode, int primary, String menuOption, String menuTarget, int mouseX, int mouseY, int trash);");
+            file.WriteLine(InlineWhiteSpace(1) + "RSObjType getItemDefinition(int itemID);");
+            file.WriteLine(InlineWhiteSpace(1) + "RSComponent getMeSlayerContinueWidget();");
+            file.WriteLine(InlineWhiteSpace(1) + "void setScene_selectedX(int x);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setScene_selectedY(int y);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setViewportWalking();");
         }
 
         private void AddExtraSceneFields(StreamWriter file)
         {
-            string whiteSpace = "    ";
-            file.WriteLine(whiteSpace + "void setDestinationX(int var1);");
-            file.WriteLine(whiteSpace + "void setDestinationY(int var1);");
-            file.WriteLine(whiteSpace + "void setWalking();");
+            file.WriteLine(InlineWhiteSpace(1) + "void setDestinationX(int var1);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setDestinationY(int var1);");
+            file.WriteLine(InlineWhiteSpace(1) + "void setWalking();");
+        }
+
+        private void AddExtraNodeDequeFields(StreamWriter file)
+        {
+            file.WriteLine(InlineWhiteSpace(1) + "default RSNode[] getNodes() {");
+            file.WriteLine(InlineWhiteSpace(2) + "ArrayList<RSNode> list = new ArrayList();");
+            file.WriteLine(InlineWhiteSpace(2) + "RSNode iter = this.getSentinel();");
+            file.WriteLine(InlineWhiteSpace(2) + "while(true) {");
+            file.WriteLine(InlineWhiteSpace(3) + "RSNode previous = iter.getPrevious();");
+            file.WriteLine(InlineWhiteSpace(3) + "if (previous == this.getSentinel()) {");
+            file.WriteLine(InlineWhiteSpace(4) + "return (RSNode[])list.toArray(new RSNode[0]);");
+            file.WriteLine(InlineWhiteSpace(3) + "}");
+            file.WriteLine(InlineWhiteSpace(3) + "iter = previous;");
+            file.WriteLine(InlineWhiteSpace(3) + "list.add(previous);");
+            file.WriteLine(InlineWhiteSpace(2) + "}");
+            file.WriteLine(InlineWhiteSpace(1) + "}");
+        }
+
+        private string InlineWhiteSpace(int count)
+        {
+            return new String(' ', 4 * count);
         }
 
         private void DeleteFiles()
--- HookInterfaceCreator/Output/RSNodeDeque.java
+++ HookInterfaceCreator/Output/RSNodeDeque.java
@@ -2,6 +2,19 @@ package api.utilities.internal;
 
 
 public interface RSNodeDeque {
+    default RSNode[] getNodes() {
+        ArrayList<RSNode> list = new ArrayList();
+        RSNode iter = this.getSentinel();
+        while(true) {
+            RSNode previous = iter.getPrevious();
+            if (previous == this.getSentinel()) {
+                return (RSNode[])list.toArray(new RSNode[0]);
+            }
+            iter = previous;
+            list.add(previous);
+        }
+    }
+
     RSNode getCurrent();
     RSNode getSentinel();
 }
