fc2f94430fe07a9163c1bcc764e039656b33ca41
LostVirt <rsvirtest@hotmail.com>
2021-03-22 07:12:56 +0100

    Add project files.

---
+++ HookInterfaceCreator/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace HookInterfaceCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject jsonInterfaces = JObject.Parse(File.ReadAllText(@"c:\videogames.json"));
+
+            
+        }
+    }
+}
